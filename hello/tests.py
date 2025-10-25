f1e54a8360674da004fb108cbde365f7cac7bf9b
szelese <szeles.ervin10@gmail.com>
2025-10-25 09:13:53 +0200

    CI quality gates

--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HelloViewTests(TestCase):
+    def test_index_view_status_code(self):
+        """
+        Teszteli, hogy a 'hello.urls'-ben 'index'-nek nevezett
+        főoldali nézet 200-as (OK) státuszkódot ad-e vissza.
+        """
+        url = reverse('index') 
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
